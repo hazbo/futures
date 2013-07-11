9221e67d1cce704ea0675ec70179a2ab287a7600
hazbo <hazbo@gmx.com>
2013-07-11 17:59:52 +0100

    Commented the URL processor class

--- src/Hazbo/Component/Http/UrlProcessor.php
+++ src/Hazbo/Component/Http/UrlProcessor.php
@@ -2,6 +2,14 @@
 
 namespace Hazbo\Component\Http;
 
+/**
+ * Url processor for futures
+ *
+ * TODO: Improve validation methods to reduce code
+ * 		 in future classes
+ *
+ * @group futures
+ */
 class UrlProcessor
 {
 	protected $url;
@@ -12,21 +20,44 @@ class UrlProcessor
 		$this->url = $url;
 	}
 
+	/**
+	 * Taken from initial URL passed through to
+	 * feature
+	 *
+	 * @param string url
+	 */
 	public function setUrl($url)
 	{
 		$this->url = $url;
 	}
 
+	/**
+	 * Get the protected url property
+	 *
+	 * @return string url
+	 */
 	public function getUrl()
 	{
 		return $this->url;
 	}
 
+	/**
+	 * Return the parts of the URL once they
+	 * have been parsed after validate method
+	 * had been called
+	 *
+	 * @return array url parts
+	 */
 	public function getUrlParts()
 	{
 		return $this->urlParts;
 	}
 
+	/**
+	 * Call all validation methods,
+	 * exceptions will be thrown if
+	 * there are any issues
+	 */
 	public function validateForHttpFuture()
 	{
 		$this->urlParts = parse_url($this->url);
@@ -35,6 +66,11 @@ class UrlProcessor
 		$this->validateHttpBasicAuth();
 	}
 
+	/**
+	 * Validates the URL scheme
+	 *
+	 * @return bool
+	 */
 	private function validateScheme()
 	{
 		$scheme = $this->urlParts['scheme'];
@@ -46,6 +82,11 @@ class UrlProcessor
         return true;
 	}
 
+	/**
+	 * Validates the URL host
+	 *
+	 * @return bool
+	 */
 	private function validateHost()
 	{
         if (!isset($this->urlParts['host'])) {
@@ -56,6 +97,12 @@ class UrlProcessor
         return true;
 	}
 
+	/**
+	 * Validates against basic auth
+	 * (Not supported using HttpFuture)
+	 *
+	 * @return bool
+	 */
 	private function validateHttpBasicAuth()
 	{
         if (isset($this->urlParts['user']) || isset($this->urlParts['pass'])) {
